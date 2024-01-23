b57e68901db2ba75de9a9f5bb8355c3ed2923687
wojtya <153980980+Wojtya@users.noreply.github.com>
2024-01-23 03:57:09 +0300

    First commit

---
+++ photoreg.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+int servoPin = 9;
+Servo myServo;
+int lightPin = A0;
+int lightVal;
+int redled = 2;
+int greenled = 6;
+int mapVal;
+
+
+void setup() {
+
+  // put your setup code here, to run once:
+  myServo.attach(servoPin);
+  pinMode(lightPin, INPUT);
+  pinMode(redled, OUTPUT);
+  pinMode(greenled, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lightVal = analogRead(lightPin);
+  Serial.println(lightVal);
+  delay(250);
+  mapVal = map(lightVal, 0, 1023, 0, 180);
+  myServo.write(mapVal);
+  if(lightVal > 200){
+    digitalWrite(greenled, HIGH);
+    delay(100);
+    digitalWrite(greenled, LOW);
+    delay(100);
+
+  }else if(lightVal < 200 ){
+    digitalWrite(redled, HIGH);
+    delay(100);
+    digitalWrite(redled, LOW);
+    delay(100);
+
+  }
+  
+}
